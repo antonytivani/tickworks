b00ee1a6c61af0ff336675aa9a30b72a0936e288
ABAT507 <anthony.tivani@absa.co.za>
2020-11-12 13:04:56 +0200

    updated styles

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit, ElementRef, HostListener, AfterViewInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  HostListener,
+  AfterViewInit,
+  ViewChild,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { EmailService } from 'src/app/services/email.service';
 import { EmailPayload } from 'src/app/models/email.payload';
-import { ToastrService } from "ngx-toastr";
+import { ToastrService } from 'ngx-toastr';
 import { MdbCardBodyComponent } from 'projects/angular-bootstrap-md/src/public_api';
-
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css'],
 })
-export class CheckOutComponent implements OnInit, AfterViewInit  {
-
+export class CheckOutComponent implements OnInit, AfterViewInit {
   elements: Product[] = [];
-  headElements = ['Product name',  'Quantity', 'Price', 'Remove'];
-  total: Number
+  headElements = ['Product name', 'Quantity', 'Price', 'Remove'];
+  total: Number;
+  totalQuantity: Number;
 
   fullname;
   contact;
@@ -27,14 +34,17 @@ export class CheckOutComponent implements OnInit, AfterViewInit  {
   comment;
   subject = 'Order';
 
-  constructor(private cartService: CartService,
-              private emailService: EmailService,
-              private toaster: ToastrService,
-              private router: Router) {}
+  constructor(
+    private cartService: CartService,
+    private emailService: EmailService,
+    private toaster: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.elements = this.cartService.cart;
-    this.total = this.cartService.totalCartPrice()
+    this.total = this.cartService.totalCartPrice();
+    this.totalQuantity = this.cartService.totalCartQuantity();
   }
 
   ngAfterViewInit() {
@@ -43,29 +53,31 @@ export class CheckOutComponent implements OnInit, AfterViewInit  {
 
   removeProductFromCart(product: Product) {
     this.cartService.removeProduct(product);
-    this.showSucess(`${product.name} removed from cart`)
+    this.showSucess(`${product.name} removed from cart`);
     this.elements = this.cartService.cart;
-    this.total = this.cartService.totalCartPrice()
-    
-    // navigate to home if products list is empty 
-    if (this.elements.length === 0) {
-      this.router.navigate(['/'])
-    }
+    this.total = this.cartService.totalCartPrice();
+    this.totalQuantity = this.cartService.totalCartQuantity();
 
+    // navigate to home if products list is empty
+    if (this.elements.length === 0) {
+      this.router.navigate(['/']);
+    }
   }
 
   placeOrder() {
     let order: string = '';
-    this.elements.forEach(product => {
-       order += `<b> Product name ${product.name} - price: R ${product.price.toFixed(2)}  - quantity: ${product.quantity} <b> <br/>`;
+    this.elements.forEach((product) => {
+      order += `<b> Product name ${product.name} - price: R ${product.price.toFixed(2)}  - quantity: ${
+        product.quantity
+      } <b> <br/>`;
     });
 
-    let body =`
+    let body = `
             <b>This is sent for an order placed.</b> <br/> 
             <b>Client Name: </b>${this.fullname} <br /> 
             <b>Client Email: </b>${this.clientEmail}<br />  
             <b>Client contact: </b>${this.contact}<br /> 
-            <b>Address : </b>${this.address }<br />  
+            <b>Address : </b>${this.address}<br />  
             <b>Message:</b> <br /> ${this.comment} <br><br> 
             ${order} <br/><br/>
             <b>~End of Message.~</b> `;
@@ -74,14 +86,14 @@ export class CheckOutComponent implements OnInit, AfterViewInit  {
       from: this.clientEmail,
       body: body,
     };
-      
-      this.showSucess("order successfull")
-      this.emailService.sendEmail(emailPayload);
-      console.log('order placed');
-      this.clearCartAndFields();
-      
-      // navigate to homepage 
-      this.router.navigate(['/'])
+
+    this.showSucess('order successfull');
+    this.emailService.sendEmail(emailPayload);
+    console.log('order placed');
+    this.clearCartAndFields();
+
+    // navigate to homepage
+    this.router.navigate(['/']);
   }
 
   clearCartAndFields() {
@@ -95,7 +107,7 @@ export class CheckOutComponent implements OnInit, AfterViewInit  {
     this.elements = [];
   }
 
-  showSucess(msg: string){
-    this.toaster.success(msg.toUpperCase())
+  showSucess(msg: string) {
+    this.toaster.success(msg.toUpperCase());
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
 
-
 @Injectable()
 export class CartService {
-
   cart: Product[] = [];
   total: number = 0;
+  totalQuantity: number = 0;
 
   constructor() {}
 
@@ -15,20 +14,26 @@ export class CartService {
   }
 
   removeProduct(product: Product): void {
-    this.cart = this.cart.filter(item => item !== product);
-  
+    this.cart = this.cart.filter((item) => item !== product);
   }
 
-  
   cartSize() {
     return this.cart.length;
   }
 
   totalCartPrice() {
     let sum: number = 0;
-    this.cart.forEach(product => {
-      sum += (product.price * product.quantity);
+    this.cart.forEach((product) => {
+      sum += product.price * product.quantity;
     });
-    return this.total = sum;
+    return (this.total = sum);
+  }
+
+  totalCartQuantity() {
+    let sum: number = 0;
+    this.cart.forEach((product) => {
+      sum += product.quantity;
+    });
+    return (this.totalQuantity = sum);
   }
 }
