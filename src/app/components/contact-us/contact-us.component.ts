eef8470d8722f681af5ffa9ea118fa562e045ffc
webHero <khonjelwayo@gmail.com>
2020-11-14 21:13:04 +0200

    styled sent email

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -20,12 +20,133 @@ export class ContactUsComponent implements OnInit {
   ngOnInit(): void {}
 
   sendMessage() {
-    let body = `
-            <b>This is sent from contact us page.</b> <br/> 
-            <b>Client Name: </b>${this.name} <br /> 
-            <b>Client Email: </b>${this.email}<br />  
-            <b>Message:</b> <br /> ${this.message} <br><br> 
-            <b>~End of Message.~</b> `;
+    let body = `<html>
+    <head>
+      
+      <style>
+        *{
+          margin: 0;
+          box-sizing: border-box;
+        }
+    
+        .myCard{
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+          display: block;
+          margin: auto;
+          max-width: 400px;
+          border: 1px solid #f1f1f1;
+          border-radius: 6px;
+          margin-top: 1em;
+          border-radius: 6px;
+          
+        }
+    
+        .header{
+          background-color: rgba(0, 0, 0, 0.9);
+          color: #fff;
+          font-size: 23px;
+          font-family: inherit;
+          border-top-left-radius: 6px;
+          border-top-right-radius: 6px;
+          text-align: center;
+          font-weight: 300;
+          margin: auto;
+          padding: .5em ;
+        }
+    
+        .content{
+          padding: 1em;
+        }
+    
+        ul {
+          list-style-type: none; 
+          padding: 1em; 
+          margin: 0; 
+        }
+    
+        ul li:first-child{
+          border-top: 1px solid #f1f1f1;
+        }
+    
+        ul li {
+          font-size: 12px;
+          border-bottom: 1px solid #f1f1f1;
+          padding: 12px; 
+        }
+    
+        ul li:last-child{
+          border-bottom: none;
+        }
+    
+        .summary, .user-heading, .comment, .comment-heading{
+          font-weight: 300;
+          color: rgba(0,0,0,0.5);
+          font-style: italic;
+        }
+        
+        .comment{
+          font-size: 14px;
+          padding: 1em;
+          color: rgba(0,0,0,0.8);
+          margin-bottom: 1em;
+        }
+        .footer{
+          background-color: #f00;
+          color: #fff;
+          padding: 1em;
+          border-bottom-left-radius: 6px;
+          border-bottom-right-radius: 6px;
+        }
+    
+        .footer span{
+          display: block; 
+          text-align: center; 
+          font-size: 18px;
+          font-weight: 500;
+        }
+    
+        .product-name{
+          font-size: 14px;
+        }
+        .product-total{
+          font-weight: 500;
+          text-align: right;
+          color: red;
+        }
+    
+        .user{
+          border-bottom: 1px solid #ccc;
+          font-size: 14px;
+          padding-bottom: 1em;
+          color: rgba(0, 0, 0, 0.5);
+          margin-bottom: 1em;
+        }
+    
+    
+        .user p{
+          text-align: center;;
+        }
+      </style>
+    </head>
+      <body>
+        <div class="myCard">
+          <h1 class="header">New Contact Request</h1>
+          <div class="content">
+            <p class="user-heading">
+              From
+            </p>
+            <div class="user">
+              <p>${this.name}</p>
+              <p>${this.email}</p>
+            </div>
+    
+            <p class="comment-heading">Message</p>
+            <p class="comment">${this.message}</p>
+               
+          </div>
+        </div>
+      </body>
+    </html>`;
     let emailPayload: EmailPayload = {
       subject: 'Contact us - ' + this.subject,
       from: this.email,
